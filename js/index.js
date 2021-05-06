08dd9269242db3bc2f7f8a98dba0abaa1f4b7858
qw753954 <68770475+qw753954@users.noreply.github.com>
2021-05-06 10:55:38 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -14,8 +14,6 @@ createApp({
       const api = 'https://vue-course-api.hexschool.io/admin/signin';
       axios.post(api, this.user).then(res => {
         console.log(res);
-        window.location = 'products.html';
-
         if (!response.data.success) return;
 
         // const { token, expired } = response.data;
@@ -24,6 +22,8 @@ createApp({
 
         // 寫入 cookie token & expires 設置有效時間
         document.cookie = `token=${token};expires=${new Date(expired)}; path=/`;
+
+        window.location = 'products.html';
       }).catch(err => {
         console.log(err.response);
       });
